5000baf48361fe8f7a8c1db95c9708b107ec5f6e
sasha <sasha@computer.(none)>
2009-03-16 22:04:40 +0300

    Это крутая штука

--- program.py
+++ program.py
@@ -8,7 +8,7 @@
 from shapes import cover, color, width, height
 
 
-cover.set_color(color.red)
+cover.set_color(color.pink)
 cover.circle(width/2,height/2,width/2-4)
 cover.fill()
 cover.set_color(color.black)
@@ -17,8 +17,15 @@ cover.fill()
 cover.set_color(color.black)
 cover.circle(width/1.5,height/3,width/9-18)
 cover.fill()
-cover.set_color(color.blue)
-cover.line(100,80)(80,140)
-cover.line(100,80)(120,140)
-cover.line(80,140)(120,140)
+cover.set_color(color.red1)
+cover.move_to(100,80)
+cover.line_to(80,140)
+cover.line_to(120,140)
+cover.line_to(100,80)
 cover.fill()
+cover.set_color(color.red)
+cover.move_to(70,170)
+cover.line_to(130,170)
+string= "Денис"
+print string
+
